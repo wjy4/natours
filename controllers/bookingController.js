03ca59bef2955dcc5173cd41884d43e94bf68add
wjy4 <w1097304549@gmail.com>
2025-04-20 22:00:37 +0100

    Fix: remove booking middleware from home page

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -45,7 +45,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 exports.createBookingCheckout = catchAsync(async (req, res, next) => {
   const { tour, user, price } = req.query;
 
-  if (!tour && !user && !price) return next();
+  if (!tour || !user || !price) return next();
   await Booking.create({ tour, user, price });
   res.redirect(req.originalUrl.split('?')[0]);
 });
