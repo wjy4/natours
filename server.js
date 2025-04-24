c4af0ab4a055c2c8d85241fc34e7fcd7940c28d2
wjy4 <w1097304549@gmail.com>
2025-04-24 13:44:24 +0100

    check which database

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(() => console.log('DB connection successful!'));
+  .then(() => console.log(`Connected to DB: ${mongoose.connection.name}`));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
