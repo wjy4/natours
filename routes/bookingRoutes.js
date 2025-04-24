7c3f20e61c2f34ec7c8e5ddc74053c46368b2081
wjy4 <w1097304549@gmail.com>
2025-04-24 11:41:38 +0100

    fix: rename getAllBooking -> getAllBookings

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -12,7 +12,7 @@ router.use(authController.restrictTo('admin', 'lead-guide'));
 
 router
   .route('/')
-  .get(bookingController.getAllBookings)
+  .get(bookingController.getAllBooking)
   .post(bookingController.createBooking);
 
 router
