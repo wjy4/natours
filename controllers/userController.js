b5d4ddfa919ddc0b91aa824c4eb525c6aedc95ee
wjy4 <w1097304549@gmail.com>
2025-04-28 10:13:08 +0100

    chore: auto deploy to heroku with precheck

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,7 +34,7 @@ exports.uploadUserPhoto = upload.single('photo');
 exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
-  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+  req.file.filename = `user-${Date.now()}.jpeg`;
 
   await sharp(req.file.buffer)
     .resize(500, 500)
