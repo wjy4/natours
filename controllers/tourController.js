602a2d0432b283d3a9729ae19f0385a5ba81e105
wjy4 <w1097304549@gmail.com>
2025-04-28 23:31:48 +0100

    chore: auto deploy to heroku with precheck

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -31,31 +31,41 @@ exports.uploadTourImages = upload.fields([
 // upload.array('images', 5);
 
 exports.resizeTourImages = catchAsync(async (req, res, next) => {
-  if (!req.files.imageCover || !req.files.images) return next();
+  if (!req.files || (!req.files.imageCover && !req.files.images)) {
+    return next();
+  }
 
-  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
-  await sharp(req.files.imageCover[0].buffer)
-    .resize(2000, 1333)
-    .toFormat('jpeg')
-    .jpeg({ quality: 90 })
-    .toFile(`public/img/tours/${req.body.imageCover}`);
+  // 处理封面图
+  if (req.files.imageCover) {
+    req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+    await sharp(req.files.imageCover[0].buffer)
+      .resize(2000, 1333)
+      .toFormat('jpeg')
+      .jpeg({ quality: 90 })
+      .toFile(`public/img/tours/${req.body.imageCover}`);
+  }
 
-  req.body.images = [];
+  // 处理多张小图
+  if (req.files.images) {
+    req.body.images = [];
 
-  await Promise.all(
-    req.files.images.map(async (file, i) => {
-      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
-      await sharp(file.buffer)
-        .resize(2000, 1333)
-        .toFormat('jpeg')
-        .jpeg({ quality: 90 })
-        .toFile(`public/img/tours/${filename}`);
+    await Promise.all(
+      req.files.images.map(async (file, i) => {
+        const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+        await sharp(file.buffer)
+          .resize(2000, 1333)
+          .toFormat('jpeg')
+          .jpeg({ quality: 90 })
+          .toFile(`public/img/tours/${filename}`);
 
-      req.body.images.push(filename);
-    }),
-  );
+        req.body.images.push(filename);
+      }),
+    );
+  }
+
   next();
 });
+
 // exports.resizeTourImages = catchAsync(async (req, res, next) => {
 //   if (!req.files.imageCover || !req.files.images) return next();
 
